fa625b3684c4937b0837928557072b3b91d71846
Mikhail <workpankovmikhail@gmail.com>
2017-11-28 20:01:38 +0700

    0.01

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+console.log('1231312');
+Ext.onReady(function() {
+Ext.create('Ext.Panel', {
+renderTo: 'helloWorldPanel',
+height: 100,
+width: 200,
+title: 'Hello world!',
+html: 'First Ext JS Hello World Program'
+});
+});
