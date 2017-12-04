b73b2811665455524b99b7dc8b8b54672f5915ae
Mikhail <workpankovmikhail@gmail.com>
2017-12-05 02:19:43 +0700

    0.02

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,51 @@
-console.log('1231312');
-Ext.onReady(function() {
-Ext.create('Ext.Panel', {
-renderTo: 'helloWorldPanel',
-height: 100,
-width: 200,
-title: 'Hello world!',
-html: 'First Ext JS Hello World Program'
+Ext.define('User', {
+extend: 'Ext.data.Model',
+fields: [{
+name: 'id',
+type: 'int'
+}, {
+name: 'name',
+type: 'string'
+}, {
+name: 'email',
+type: 'string'
+}]
 });
+
+
+Ext.onReady(function() {
+
+var user = Ext.create('Ext.data.Store', {
+storeId: 'user',
+model: 'User',
+autoLoad: 'true',
+proxy: {
+type: 'ajax',
+url: 'js/users.json',
+reader: {
+type: 'json',
+root: 'blah'
+}
+}
+});
+
+Ext.create('Ext.grid.Panel', {
+store: user,
+id: 'user',
+title: 'Users',
+columns: [{
+header: 'ID',
+dataIndex: 'id'
+}, {
+header: 'NAME',
+dataIndex: 'name'
+}, {
+header: 'Email',
+dataIndex: 'email'
+}],
+height: 300,
+width: 400,
+renderTo: Ext.getBody()
+});
+
 });
